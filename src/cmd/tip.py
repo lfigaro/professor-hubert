0d0e429cdd6842454ff9eb955dc515e07f0da99e
Figaro <luis.figaro@vivareal.com>
2019-01-04 16:27:04 -0200

    Implementing specialized tips

--- src/cmd/tip.py
+++ src/cmd/tip.py
@@ -3,6 +3,7 @@
 import ConfigParser
 import random
 import sys
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -11,10 +12,21 @@ class Tip:
 
 	def __init__(self, help, message):
 		self.text = message
+		self.subject = None
 
 	def execute(self):
 		config = ConfigParser.RawConfigParser()
 		config.read('command.cfg')
-		ret = config.items('command-tip')
+		tips = config.items('command-tips')
+
+		if self.subject is not None:
+			ret = []
+			for key, value in tips:
+				matchObj = re.search( key, self.subject , re.M|re.I )
+				
+				if matchObj:
+					ret.append( value )
+		else:
+			ret = tips
 
 		return ret[random.randint(0,len(ret)-1)]
